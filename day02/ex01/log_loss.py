360da699bb26d9a614844045847228b3dc8cf484
Patrice Cadiot <pathibul.r@gmail.com>
2020-01-22 16:11:26 +0100

    day02 ex01, ex02, ex03 and ex04 done

---
+++ day02/ex01/log_loss.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import numpy as np
+
+def log_loss_(y_true, y_pred, m, eps=1e-15):
+    """
+    Compute the logistic loss value.
+    Args:
+    y_true: a scalar or a list for the correct labels
+    y_pred: a scalar or a list for the predicted labels
+    m: the length of y_true (should also be the length of y_pred)
+    eps: eps (default=1e-15)
+    Returns:
+    The logistic loss value as a float.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    y_t, y_p = np.array(y_true), np.array(y_pred)
+    cost = (1 / m) * (((-y_t).T * np.log(y_p + eps)) - ((1 - y_t).T * np.log(1 - y_p + eps)))
+    # cost = (1 / m) * (((-y_t).T @ np.log(y_p + eps)) - ((1 - y_t).T @ np.log(1 - y_p + eps)))
+    return cost if isinstance(cost, float) else cost.sum()
+
+
+if __name__ == '__main__':
+    # from sigmoid import sigmoid_
+    # from log_loss import log_loss_
+
+    def sigmoid_(x):
+        """
+        Compute the sigmoid of a scalar or a list.
+        Args:
+        x: a scalar or list
+        Returns:
+        The sigmoid value as a scalar or list.
+        None on any error.
+        Raises:
+        This function should not raise any Exception.
+        """
+        x = np.asarray(x)
+        sigm = 1. / (1. + np.exp(-x))
+        return sigm
+    # Test n.1
+    x = 4
+    y_true = 1
+    theta = 0.5
+    y_pred = sigmoid_(x * theta)
+    m = 1  # length of y_true is 1
+    print(log_loss_(y_true, y_pred, m))
+    # 0.12692801104297152
+    # Test n.2
+    x = [1, 2, 3, 4]
+    y_true = 0
+    theta = [-1.5, 2.3, 1.4, 0.7]
+    x_dot_theta = sum([a * b for a, b in zip(x, theta)])
+    y_pred = sigmoid_(x_dot_theta)
+    m = 1
+    print(log_loss_(y_true, y_pred, m))
+    # 10.100041078687479
+    # Test n.3
+    x_new = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+    y_true = [1, 0, 1]
+    theta = [-1.5, 2.3, 1.4, 0.7]
+    x_dot_theta = []
+    for i in range(len(x_new)):
+        my_sum = 0
+        for j in range(len(x_new[i])):
+            my_sum += x_new[i][j] * theta[j]
+        x_dot_theta.append(my_sum)
+    y_pred = sigmoid_(x_dot_theta)
+    m = len(y_true)
+    print(log_loss_(y_true, y_pred, m))
+    # 7.233346147374828
---
+++ day02/ex02/log_gradient.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import numpy as np
+
+
+
+def log_gradient_(x, y_true, y_pred):
+    """
+    Compute the gradient.
+    Args:
+    x: a list or a matrix (list of lists) for the samples
+    y_true: a scalar or a list for the correct labels
+    y_pred: a scalar or a list for the predicted labels
+    Returns:
+    The gradient as a scalar or a list of the width of x.
+    None on any error.
+    Raises:
+    This function should not raise any Exception.
+    """
+    X = np.array(x)
+    y_t, y_p = np.array(y_true), np.array(y_pred)
+    # gradient = np.dot(X.T, (y_p - y_t)) / y_t.shape[0]
+    # gradient = ((y_p - y_t) * X.T) / X.shape[0]
+    gradient = np.dot((y_p - y_t),  X) #/ y_t.size
+    return gradient
+
+if __name__ == '__main__':
+
+    def sigmoid_(x):
+        """
+        Compute the sigmoid of a scalar or a list.
+        Args:
+        x: a scalar or list
+        Returns:
+        The sigmoid value as a scalar or list.
+        None on any error.
+        Raises:
+        This function should not raise any Exception.
+        """
+        x = np.asarray(x)
+        sigm = 1. / (1. + np.exp(-x))
+        return sigm
+    # Test n.1
+    x = [1, 4.2]  # 1 represent the intercept
+    y_true = 1
+    theta = [0.5, -0.5]
+    x_dot_theta = sum([a * b for a, b in zip(x, theta)])
+    y_pred = sigmoid_(x_dot_theta)
+    print(log_gradient_(x, y_pred, y_true))
+    # [0.8320183851339245, 3.494477217562483]
+
+    # Test n.2
+    x = [1, -0.5, 2.3, -1.5, 3.2]
+    y_true = 0
+    theta = [0.5, -0.5, 1.2, -1.2, 2.3]
+    x_dot_theta = sum([a * b for a, b in zip(x, theta)])
+    y_pred = sigmoid_(x_dot_theta)
+    print(log_gradient_(x, y_true, y_pred))
+    # [0.99999685596372, -0.49999842798186, 2.299992768716556, -1.4999952839455801, 3.1999899390839044]
+
+    # Test n.3
+    x_new = [[1, 2, 3, 4, 5], [1, 6, 7, 8, 9], [1, 10, 11, 12, 13]]
+    # first column of x_new are intercept values initialized to 1
+    y_true = [1, 0, 1]
+    theta = [0.5, -0.5, 1.2, -1.2, 2.3]
+    x_new_dot_theta = []
+    for i in range(len(x_new)):
+        my_sum = 0
+        for j in range(len(x_new[i])):
+            my_sum += x_new[i][j] * theta[j]
+        x_new_dot_theta.append(my_sum)
+    y_pred = sigmoid_(x_new_dot_theta)
+    print(log_gradient_(x_new, y_true, y_pred))
+    # [0.9999445100449934, 5.999888854245219, 6.999833364290213, 7.999777874335206, 8.999722384380199]
